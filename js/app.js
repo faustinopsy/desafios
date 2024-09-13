a12ecd21981d4ade14d964e4bbd8b5ff49c9cb12
faustinopsy <rodrigohipnose@gmail.com>
2024-09-13 11:35:45 -0300

    modified:   index.html 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -6,12 +6,24 @@ document.addEventListener("DOMContentLoaded", ()=>{
         ["smartphone", "fones de ouvido", "capa de celular"],
         ["teclado", "mouse", "tapete de mouse"]
     ];
-    let text ='';
-    const main = document.querySelector("main")
-    historicoCompras.forEach(listarItem)
-    main.innerHTML = text
+   
+    const input = document.querySelector("#produto")
+    const lista = document.querySelector("#lista")
 
-function listarItem(item, index) {
-    text += index + ": " + item + "<br>"; 
-}
+    input.addEventListener("blur", (e)=>{
+       const produto = e.target.value.toLowerCase();
+       let text ='';
+
+       const historico = historicoCompras.values();
+       for (let item of historico) {
+            if(item.includes(produto)){
+                let index = item.indexOf(produto);
+                item.splice(index,1)
+                    text += item + "<br>";
+            }
+       }
+         
+        lista.innerHTML = `<b>Produtos recomendados:</b> <br>${text}`
+    })
+  
 })
